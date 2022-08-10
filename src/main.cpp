cb44b3402e6e16c721f22fdf04c3a11597176a94
Ricardo <ricarditomontserrat@gmail.com>
2022-08-10 22:26:53 +0200

    Simplify code into main, forget about classes unless necessary

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,25 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 
-#include "RenderWindow.hpp"
 #include "Logger.h"
 
-int main(int argv, char *args[])
+int main()
 {
-    if (SDL_Init(SDL_INIT_VIDEO) > 0)
-        LOG("SDL_Init HAS FAILED!!! " << SDL_GetError());
-    if (!(IMG_Init(IMG_INIT_PNG)))
-        LOG("IMG_Init HAS FAILED!!! " << SDL_GetError());
+    if (SDL_Init(SDL_INIT_VIDEO) > 0) LOG("SDL_Init HAS FAILED!!! " << SDL_GetError());
+//    if (!(IMG_Init(IMG_INIT_PNG))) LOG("IMG_Init HAS FAILED!!! " << SDL_GetError());
 
-    LOG("It Runs!");
+    SDL_Window *window = SDL_CreateWindow("Game v0.1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 640, SDL_WINDOW_SHOWN);
 
-    RenderWindows windows("GAME v0.1", 800, 640);
-
-    bool gameRunning = true;
+    if (window == NULL) LOG("Window Failed to Init " << SDL_GetError());
 
     SDL_Event event;
 
-    while (gameRunning)
+    while (SDL_PollEvent(&event) || event.type != SDL_QUIT)
     {
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT) gameRunning = false;
-        }
     }
+
+    SDL_DestroyWindow(window);
+
+    LOG("Windowed destroyed!");
 
     SDL_Quit();
 
